91ff53e6a494fef16abb82fca4a1a5b2c7f5694a
Tajinder <tajinderpalsingh61@gmail.com>
2023-01-02 09:37:29 +0530

    rolling-average-tweets

---
+++ SQL/Medium/rolling-average-tweets.sql
@@ -0,0 +1,14 @@
+-- https://datalemur.com/questions/rolling-average-tweets
+
+SELECT user_id, tweet_date,
+round(avg(tweet_cnt) over(PARTITION BY user_id order by tweet_date
+rows between 2 preceding and current row
+), 2) as rolling_avg_3days
+FROM
+(
+SELECT user_id , tweet_date, count(tweet_id) as tweet_cnt
+FROM tweets
+group by user_id , tweet_date
+) as a
+order by user_id, tweet_date
+;
